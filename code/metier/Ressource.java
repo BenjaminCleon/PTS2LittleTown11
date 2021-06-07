cc36e13edc04a16119ec59d79df66adbe7901886
Cerbultubage99 <76993234+Cerbultubage99@users.noreply.github.com>
2021-06-07 17:49:11 +0200

    Update Ressource.java

--- code/metier/Ressource.java
+++ code/metier/Ressource.java
@@ -15,7 +15,6 @@ public class Ressource extends Tuile
 
 	/**
 	 * C'est la quantite de Ble disponible dans le stock.
-	 * Ici, le préfixe is correspond à "int" selon la notation hongroise.
 	 *
 	 * @see Ressource#getRessourceByType( String )
 	 * @see Ressource#toString()
@@ -24,7 +23,6 @@ public class Ressource extends Tuile
 	
 	/**
 	 * C'est la quantite de Bois disponible dans le stock.
-	 * Ici, le préfixe is correspond à "int" selon la notation hongroise.
 	 *
 	 * @see Ressource#getRessourceByType( String )
 	 * @see Ressource#toString()
@@ -33,7 +31,6 @@ public class Ressource extends Tuile
 	
 	/**
 	 * C'est la quantite d'Eau disponible dans le stock.
-	 * Ici, le préfixe is correspond à "int" selon la notation hongroise.
 	 *
 	 * @see Ressource#getRessourceByType( String )
 	 * @see Ressource#toString()
@@ -42,18 +39,46 @@ public class Ressource extends Tuile
 	
 	/**
 	 * C'est la quantite de Pierre disponible dans le stock.
-	 * Ici, le préfixe is correspond à "int" selon la notation hongroise.
 	 *
 	 * @see Ressource#getRessourceByType( String )
 	 * @see Ressource#toString()
 	 */
 	private static AltInt iQtePierre = new AltInt( 15 );	
 
-	private int iQteRessource;
-
+	/**
+	 * C'est le nom de la ressource.
+	 *
+	 * @see Ressource#getRessourceByType( String )
+	 * @see Ressource#toString()
+	 * @see Ressource#Ressource( String, boolean bEstMangeable )
+	 * @see Ressource#Ressource( String )
+	 * @see Ressource#getRessourceByType( String  )
+	 * @see Ressource#consommerRessource( int, String  )
+	 * @see Ressource#ajouterRessource( int, String )
+	 * @see Ressource#getType()
+	 * @see Ressource#toString()
+	 */
 	private String  sType;
+	
+	/**
+	 * Esque cette ressource peut etre utiliser pour nourire les ouvriers.
+	 *
+	 * @see Ressource#getEstMangeable()
+	 */
 	private boolean bEstMangeable;
 
+	/*--------------*/
+	/* Constructeur */
+	/*--------------*/
+
+	/**
+	 * Constructeur de Ressources.
+	 *
+	 * @param sType
+	 *          Nom de la nouvelle Ressource.
+	 * @param bEstMangeable
+	 *          Definie si cette ressource peut etre utiliser pour nourire les ouvriers.
+	 */
 	public Ressource( String sType, boolean bEstMangeable )
 	{
 		super( "Ressource" );
@@ -61,11 +86,31 @@ public class Ressource extends Tuile
 		this.bEstMangeable = bEstMangeable;
 	}
 
+package littletown.metier;
+
+import littletown.metier.utilitaire.AltInt;
+
+	/**
+	 * Constructeur par recopie de Ressources
+	 * bEstMangeable est defini sur false par default
+	 *
+	 * @param sType
+	 *          Nom de la nouvelle Ressource.
+	 */
 	public Ressource( String sType )
 	{
 		this( sType, false );
 	}
 
+	/**
+	 * Retourne la bonne Ressource pour chaque nom.
+	 *
+	 * @param sType
+	 *          Nom de la ressource à rechercher.
+	 * 
+	 * @see Ressource#consommerRessource
+	 * @see Ressource#ajouterRessource
+	 */
 	private AltInt getRessourceByType( String sType )
 	{
 		switch ( sType.toUpperCase() )
@@ -78,9 +123,18 @@ public class Ressource extends Tuile
 		}
 	}
 
-	public boolean consommerRessource( int iConso )
+	/**
+	 * Consomme une quantiter d'une ressource passer en parametre.
+	 *
+	 * @param iConso
+	 *          nombre de ressource à consommer
+	 * 
+	 * @param sType
+	 *          nom de la ressource à consommer
+	 */
+	public boolean consommerRessource( int iConso, String sType )
 	{
-		AltInt tmp = this.getRessourceByType( this.sType );
+		AltInt tmp = this.getRessourceByType( sType );
 
 		if ( iConso < 1 || iConso > tmp.getEntier() )return false;
 
@@ -88,9 +142,18 @@ public class Ressource extends Tuile
 		return true;	
 	}
 
-	public boolean ajouterRessource( int iConso )
+	/**
+	 * Ajoute une quantiter à une ressource passer en parametre.
+	 *
+	 * @param iConso
+	 *          nombre de ressource à ajoute
+	 * 
+	 * @param sType
+	 *          nom de la ressource à ajoute
+	 */
+	public boolean ajouterRessource( int iConso, String sType )
 	{
-		AltInt iTmp = this.getRessourceByType( this.sType );
+		AltInt iTmp = this.getRessourceByType( sType );
 
 		if ( iConso < 1 || iTmp.getEntier() + iConso > 15 )return false;
 
@@ -99,10 +162,62 @@ public class Ressource extends Tuile
 		return true;	
 	}
 
+	/**
+	 * Retourne le nom de la ressource.
+	 */
 	public String  getType()        { return this.sType;         }
+	
+	/**
+	 * Retourne si la ressource peut etre utiliser pour nourire les ouvriers.
+	 */
 	public boolean getEstMangeable(){ return this.bEstMangeable; }
-	public int     getQteRessource(){ return this.iQteRessource; }
+	
+	/**
+	 * Retourne le nombre de ble dans le stock.
+	 *
+	 * @see Ressource#toString()
+	 */
+	public int     getQteBle(){ return this.iQteBle.getEntier(); }
+	
+	/**
+	 * Retourne le nombre de bois dans le stock.
+	 *
+	 * @see  Ressource#toString()
+	 */
+	public int     getQteBois(){ return this.iQteBois.getEntier(); }
+	
+	/**
+	 * Retourne le nombre d'Eau dans le stock.
+	 *
+	 * @see  Ressource#toString()
+	 */
+	public int     getQteEau(){ return this.iQteEau.getEntier(); }
+	
+	/**
+	 * Retourne le nombre de Pierre dans le stock.
+	 *
+	 * @see  Ressource#toString()
+	 */
+	public int     getQtePierre(){ return this.iQtePierre.getEntier; }
+	
+	/**
+	 * Retourne le nombre de ressource disponible dans le stock pour cette ressource.
+	 */
+	public int     getQteRessource()
+	{
+		switch ( this.sType.toUpperCase() )
+		{
+			case "BLE"    -> { return Ressource.iQteBle   .getEntier(); }
+			case "BOIS"   -> { return Ressource.iQteBois  .getEntier(); }
+			case "EAU"    -> { return Ressource.iQteEau   .getEntier(); }
+			case "PIERRE" -> { return Ressource.iQtePierre.getEntier(); }
+			default       -> { return null;                             }
+		}
+	}
 
+	/**
+	 * Retourne en String le nom de la ressource et le quantiter restant dans le stock.
+	 */
 	public String  toString()
 	{
 		String sRet = "";
@@ -118,3 +233,4 @@ public class Ressource extends Tuile
 		return sRet;
 	}
 }
+
