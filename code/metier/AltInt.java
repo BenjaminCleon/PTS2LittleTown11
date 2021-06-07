1a872ecb1333e51522e5183410ffc951a2f680ed
LilBrainMeiz <enzo.loubeau@etu.univ-lehavre.fr>
2021-06-07 15:59:35 +0200

    Ajout du package utilitaire

--- code/metier/AltInt.java
+++
@@ -1,9 +0,0 @@
-public class AltInt
-{
-	int entier;
-
-	public AltInt( int entier ){this.entier = entier;}
-
-	public void setEntier( int entier ){ this.entier = entier;}
-	public  int getEntier()            { return this.entier  ;}
-}	
