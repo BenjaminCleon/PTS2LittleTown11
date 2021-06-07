a5368575de5163a9fc8f2ef18e24b3fd5fba3ef0
Mathis Pesquerel <pm201742@univ-lehavre.fr>
2021-06-07 16:03:19 +0200

    Ajout des classes CartesObjetctifs et Pion

---
+++ code/metier/CartesObjectifs.java
@@ -0,0 +1,18 @@
+//import java.awt.;
+
+public class CartesObjectifs
+{
+    private int     iScore;
+    private String  sObjectif;
+
+    public CartesObjectifs ( int iScore, String sObjectif )
+    {
+        this.iScore    = iScore;
+        this.sObjectif = sObjectif;
+    }
+
+    public boolean bAccompli ()
+    {
+        
+    }
+}
