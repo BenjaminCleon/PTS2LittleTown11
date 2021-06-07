31a9dc941bd9b96aec8eb66f7732c45c6465f1ae
LilBrainMeiz <enzo.loubeau@etu.univ-lehavre.fr>
2021-06-07 15:59:56 +0200

    Ajout du nouveau package utilitaire

---
+++ code/metier/utilitaire/AltInt.java
@@ -0,0 +1,11 @@
+package littletown.metier.utilitaire;
+
+public class AltInt
+{
+	int entier;
+
+	public AltInt( int entier ){this.entier = entier;}
+
+	public void setEntier( int entier ){ this.entier = entier;}
+	public  int getEntier()            { return this.entier  ;}
+}	
